747ee5bff191250a632f132868f6f0565759c72f
Marcos Rezende <rezehnde@gmail.com>
2019-11-19 19:52:06 -0300

    First version

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once(__DIR__.'/vendor/autoload.php');
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/templates');
+$twig = new Twig\Environment($loader);
+echo $twig->render('index.twig');
