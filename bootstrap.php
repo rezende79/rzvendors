525c2d730252ecb5c8ada9b4516d500ea4278bc8
Marcos Rezende <rezehnde@gmail.com>
2019-12-07 09:47:41 -0300

    Refactoring autoload

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,4 @@
 <?php
 require_once(__DIR__.'/vendor/autoload.php');
-
-require_once(__DIR__.'/src/model/DatabaseObject.php');
-require_once(__DIR__.'/src/model/Company.php');
-
 $loader = new Twig\Loader\FilesystemLoader(__DIR__.'/src/view');
 $twig = new Twig\Environment($loader);
